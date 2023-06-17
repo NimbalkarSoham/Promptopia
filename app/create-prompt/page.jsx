9bb295527b4d87f8604870c4c95c7ebd2882d9bd
Soham Nimbalkar <soham.nimbalkar08@gmail.com>
2023-06-17 15:01:29 +0530

    backend routing complete

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,19 +1,42 @@
 'use client';
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { Router } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
+    const router = useRouter();
+    const {data: session } = useSession();
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
         tag: '',
     });
 
-    const createPrompt = (e) => {
+    const createPrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+        
+        try {
+            const response = await fetch('api/prompt/new',{
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag,
+                })
+            })
 
+            if(response.ok){
+                router.push('/');
+            }
+        } catch (error) {
+            console.log(error);
+        } finally{
+            setSubmitting(false);
+        }
     }
   return (
     <Form 
@@ -21,7 +44,7 @@ const CreatePrompt = () => {
         post = {post}
         setPost = {setPost}
         submitting = {submitting}
-        handleSubmit = {CreatePrompt}
+        handleSubmit = {createPrompt}
     />
   )
 }
